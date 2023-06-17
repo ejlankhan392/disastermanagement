809d62f34155cd8050b97a6e07c6d42cca410b29
Ejlankhan <1bo20cs036.ejlanwasiqullakhan@brindavancollege.com>
2023-06-17 21:20:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/disaster/MainActivityj.java
@@ -0,0 +1,87 @@
+package com.example.disaster;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.disaster.databinding.ActivityMainBinding;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
+
+public class MainActivityj extends AppCompatActivity {
+
+    private RelativeLayout homeRL;
+    private ProgressBar loadingPB;
+    private TextView cityNameTv, temperatureTV, conditionTV;
+    private RecyclerView weatherRV;
+    private TextInputEditText cityEdt;
+    private ImageView backIV, iconIV, searchIV;
+    private ArrayList<WeatherRVModel> weatherRVModelArrayList;
+    private WeatherRVAdapter weatherRVAdapter;
+    private LocationManager locationManager;
+    private int PERMISSION_CODE = 1;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        setContentView(R.layout.activity_main);
+        homeRL = findViewById(R.id.idRLHome);
+        loadingPB = findViewById(R.id.idPBLoading);
+        cityNameTv = findViewById(R.id.idTVCityName);
+        temperatureTV = findViewById(R.id.idTVTemperature);
+        conditionTV = findViewById(R.id.idTVCondition);
+        weatherRV = findViewById(R.id.idRvWeather);
+        cityEdt = findViewById(R.id.idEdtCity);
+        backIV = findViewById(R.id.idIVBack);
+        iconIV = findViewById(R.id.idIVIcon);
+        searchIV = findViewById(R.id.idIVSearch);
+        weatherRVModelArrayList = new ArrayList<>();
+        weatherRVAdapter = new WeatherRVAdapter(this, weatherRVModelArrayList);
+        weatherRV.setAdapter(weatherRVAdapter);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED);
+        ActivityCompat.requestPermissions(MainActivityj.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_CODE);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+    private void getWeatherInfo(String cityName) {
+        String url = "http://api.weatherapi.com/v1/forecast.json?key=4777144d25cd4264b64193828231506&q=Bangalore&days=1&aqi=no&alerts=no";
+    }
+
+}
